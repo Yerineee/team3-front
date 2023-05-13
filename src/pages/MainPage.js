d6092f50ce587bd80a3169dafe54cdad2db4c5ec
jiwoorld <113344660+jiwoorld@users.noreply.github.com>
2023-05-13 19:03:43 +0900

    ✨ feat:Header 파일 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,12 @@
+import Header from "../components/Footer/Header";
+
 const MainPage = () => {
-  return <div>메인 페이지</div>;
+  return (
+    <div>
+      <Header />
+      <div>메인 페이지</div>
+    </div>
+  );
 };
 
 export default MainPage;
