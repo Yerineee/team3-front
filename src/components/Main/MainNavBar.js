03e766f5272c934bd79668247ece472bdea46e23
jiwoorld <bncmdbev@gmail.com>
2023-05-16 00:58:04 +0900

    💄 style-components 추가

--- src/components/Main/MainNavBar.js
+++ src/components/Main/MainNavBar.js
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
 const MainNavBar = () => {
-  return <div>MainNavBar</div>;
+  return (
+    <Div>
+      <div>MainNavBar</div>
+    </Div>
+  );
 };
 
 export default MainNavBar;
+
+const Div = styled.div`
+  position: relative;
+  top: 0px;
+
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 141px;
+  background-color: #00dc64;
+`;
