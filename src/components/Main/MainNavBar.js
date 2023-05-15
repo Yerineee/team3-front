1ec7351fbc41219fff55d8b94d5046390ebe1130
jiwoorld <bncmdbev@gmail.com>
2023-05-16 01:44:31 +0900

    💄 MainNavBar icon 스타일 수정

--- src/components/Main/MainNavBar.js
+++ src/components/Main/MainNavBar.js
@@ -1,9 +1,18 @@
 import styled from "styled-components";
+import cookie from "../../assets/Main/cookie.png";
+import logo from "../../assets/Main/logo.png";
+import profile from "../../assets/Main/profile.png";
+import search from "../../assets/Main/search.png";
 
 const MainNavBar = () => {
   return (
     <Div>
-      <div>MainNavBar</div>
+      <div>
+        <ProfileIcon src={profile} />
+        <MainLogo src={logo} />
+        <CookieIcon src={cookie} />
+        <SearchIcon src={search} />
+      </div>
     </Div>
   );
 };
@@ -20,4 +29,41 @@ const Div = styled.div`
   width: 100%;
   height: 141px;
   background-color: #00dc64;
+`;
+
+const MainLogo = styled.img`
+  position: absolute;
+  width: 120px;
+  height: 30px;
+  left: calc(50% - 120px / 2 - 1px);
+  top: 79px;
+`;
+
+const ProfileIcon = styled.img`
+  position: absolute;
+  width: 24px;
+  height: 24px;
+
+  left: 26px;
+  top: 82px;
+`;
+
+const CookieIcon = styled.img`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+
+  right: 55.76px;
+  top: 86.64px;
+`;
+
+const SearchIcon = styled.img`
+  position: absolute;
+  width: 18.93px;
+  height: 19.85px;
+
+  right: 24.92px;
+  top: 86.64px;
+
+  transform: rotate(-14.28deg);
 `;
