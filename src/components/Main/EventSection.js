4b5713f96a92534c082ea1607b7f2241ee06cde4
jiwoorld <bncmdbev@gmail.com>
2023-05-17 20:18:49 +0900

    🔥 EventSlide 파일 삭제

--- src/components/Main/EventSection.js
+++ src/components/Main/EventSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import styled from "styled-components";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
