0f5f3176336e327d8b2ffe36f6676e2d56b8e85a
jiwoorld <bncmdbev@gmail.com>
2023-05-17 20:49:00 +0900

    ✨ baseURL 설정

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://server.efubtoy3.net";
+
+export const API = {
+  GET_EVENT: `${BASE_URL}/events`,
+  GET_NOVELS_RECENTLY: `${BASE_URL}/novels/recently`,
+  GET_NOVELS_FREELIST: `${BASE_URL}/novels/freeList?`,
+  GET_COMICS_RECENTLY: `${BASE_URL}/comics/recently`,
+  GET_COMICS_FREELIST: `${BASE_URL}/comics/freeList?`,
+};
