063968ff558519b7a2c361b1cb75d4ff4698e493
jiwoorld <bncmdbev@gmail.com>
2023-05-23 09:27:31 +0900

    ✨ 상세정보 페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<MainPage />} />
+        <Route path="/contents/:contentId/details" element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
   );
